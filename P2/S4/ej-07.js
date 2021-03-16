199513a8edce743af93906d97413ee3e8ea0374b
anavilla <a.villanueva.2018@alumnos.urjc.es>
2021-03-16 14:05:31 +0100

    Ejemplo 7 S4

---
+++ P2/S4/ej-07.js
@@ -0,0 +1,9 @@
+console.log("Ejecutando js")
+
+const test = document.getElementById('test')
+
+/* Propia asignacion del manejador */
+
+test.onclick = function(){
+    console.log("click en el párrafo")
+}
