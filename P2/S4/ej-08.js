7536d046d27b6864957be3485f3bbd43150395c9
anavilla <a.villanueva.2018@alumnos.urjc.es>
2021-03-16 14:08:22 +0100

    Ejemplo 8 S4

---
+++ P2/S4/ej-08.js
@@ -0,0 +1,10 @@
+console.log("Ejecutando js")
+
+const test = document.getElementById("test")
+
+/* Anotacion mas compacta para expresar
+accion */
+
+test.onclick = () => {
+    console.log("Click sobre párrafo")
+}
