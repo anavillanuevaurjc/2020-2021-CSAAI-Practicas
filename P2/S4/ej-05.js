863aa9e7eb4b6fe8aecf1bcfe8f587e245b5761c
anavilla <a.villanueva.2018@alumnos.urjc.es>
2021-03-16 14:04:52 +0100

    Ejemplo 5 S4

---
+++ P2/S4/ej-05.js
@@ -0,0 +1,9 @@
+/* Deseamos identificar elemento con
+identificador denominado test */
+
+const test = document.getElementById('test')
+
+/* Mostrar por la consola el contenido del párrafo */
+
+console.log("Párrafo test leido. Dice:")
+console.log(test.innerHTML) /* accede a la informacion */
